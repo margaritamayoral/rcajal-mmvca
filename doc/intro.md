f0dbb6409b69bacf2bd7489971f2679e68cd65da
Margarita Mayoral Villa <margaritamayoralvilla@Margaritas-MacBook-Air.local>
2017-04-22 10:01:47 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rcajal-mmvca
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rcajal_mmvca/core_test.clj
@@ -0,0 +1,7 @@
+(ns rcajal-mmvca.core-test
+  (:require [clojure.test :refer :all]
+            [rcajal-mmvca.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
